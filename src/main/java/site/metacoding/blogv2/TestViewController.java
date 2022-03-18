f55bbda4b96eb6ee86466e8c8f3a681dca4077f1
tjdgus7562 <tjdgus7562@naver.com>
2022-03-18 15:59:21 +0900

    2.컨트롤러 페이지 작동 테스트완료 및 썸머노트 cdn연결

---
+++ src/main/java/site/metacoding/blogv2/TestViewController.java
@@ -0,0 +1,49 @@
+package site.metacoding.blogv2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestViewController {
+
+    @GetMapping("/post/list")
+    public String list() {
+        return "post/list";
+    }
+
+    @GetMapping("/post/detail")
+    public String detail() {
+        return "post/detail";
+    }
+
+    @GetMapping("/post/updateForm")
+    public String updateForm() {
+        return "post/updateForm";
+    }
+
+    @GetMapping("/post/writeForm")
+    public String writeForm() {
+        return "post/writeForm";
+    }
+
+    @GetMapping("/user/detail")
+    public String detail2() {
+        return "user/detail";
+    }
+
+    @GetMapping("/user/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/user/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+    @GetMapping("/user/updateForm")
+    public String updateForm2() {
+        return "user/updateForm";
+    }
+
+}
